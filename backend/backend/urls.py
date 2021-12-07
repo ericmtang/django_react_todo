c839aaabbf4c84bc83cfe7a9ffeaffb125717443
ericmtang <ericmtang@gmail.com>
2021-12-06 22:51:00 -0800

    test pull

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from todo import views
+#from core.views import index
 
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
@@ -26,4 +27,5 @@ print('PRINTING', router.urls)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+#    path("", index, name="index"),
 ]
