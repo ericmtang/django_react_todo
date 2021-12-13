9ff7079b805af8fb7737d5351b34c179a1400444
ericmtang <ericmtang@gmail.com>
2021-12-13 14:24:39 -0800

    Added React Test 2

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,8 +3,20 @@ import App from './App';
 
 test('renders ToDo App screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/ToDo App/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/ToDo App/i);
+  expect(title).toBeInTheDocument();
 });
 
+test('renders Add Task button', () => {
+  render(<App />);
+  const addButton = screen.getByRole('button', {name: /Add Task/i});
+  expect(addButton).toBeInTheDocument();
+});
 
+// test('renders Milk todo item', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Get Some Milk/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+afterAll(() => cleanup());
